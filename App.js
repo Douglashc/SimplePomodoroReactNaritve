78456de37bdf78b28de9696a449c2a41d3b0739c
Douglashc <douglash.dcz@gmail.com>
2024-03-16 09:05:20 -0400

    Se subio la app

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
+import { useState, useEffect } from 'react'; 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Platform, TouchableOpacity } from 'react-native';
+import { Audio } from 'expo-av';
+import Header from './src/components/Header';
+import Timer from './src/components/Timer';
+
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['new NativeEventEmitter']); // Ignore log notification by message
+LogBox.ignoreAllLogs(); //Ignore all log notifications
+
+const conlors = ["#F7DC6F", "#A2D9CE", "#D7BDE2"];
 
 export default function App() {
+
+  const [isWorking, setIsWorking] = useState(false);
+  const [time, setTime] = useState(25 * 60);
+  const [currentTime, setCurrentTime] = useState("POMO" | "SHORT" |"BREAK");
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+
+    if(isActive){
+      interval = setInterval(() => {
+        setTime(time - 1);
+      }, 1000);
+    }else{
+      clearInterval(interval);
+    }
+
+    if(time === 0){
+      setIsActive(false);
+      setIsWorking((prev) => !prev);
+      setTime(isWorking ? 300 : 1500);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive, time]);
+
+  function handleStartStop(){
+    playSound();
+    setIsActive(!isActive);
+  }
+  
+  async function playSound() {
+    const { sound } = await Audio.Sound.createAsync(
+      require('./assets/click.mp3')
+    );
+    await sound.playAsync();
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={[styles.container, {backgroundColor: conlors[currentTime]}]}>
+      <View style={{ flex: 1, paddingHorizontal: 15, paddingTop: Platform.OS === 'android' && 30 }}>
+        <Text style={styles.text}>Pomodoro</Text>
+        <Header 
+          setTime={setTime}
+          isActive={isActive}
+          currentTime={currentTime}
+          setCurrentTime={setCurrentTime} 
+        />
+        <Timer time={time} />
+        <TouchableOpacity style={styles.button} onPress={handleStartStop}>
+          <Text style={{color: "white", fontWeight: "bold"}}>
+            {isActive ? "STOP" : "START"}
+          </Text>
+        </TouchableOpacity>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  text: {
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#333333',
+    padding: 15,
+    marginTop: 15,
+    borderRadius: 15,
+  }
 });
---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native"; 
+
+const options = ["Pomodoro", "Short Break", "Long Break"];
+
+export default function Header({ currentTime, setCurrentTime, setTime, isActive }) {
+    
+    function handlePress(index){
+        const newTime = index === 0 ? 25 : index === 1 ? 5 : 15;
+        setCurrentTime(index);
+        setTime(newTime * 60);
+    }
+    
+    return (
+        <View style={{ flexDirection: 'row' }}>
+            {options.map((item, index) => (
+                <TouchableOpacity 
+                    key={index} style={[styles.itemStyle, currentTime !== index && {borderColor: 'transparent'}]}
+                    onPress={() => handlePress(index)}
+                    disabled={isActive ? true : false}
+                >
+                    <Text style={{ fontWeight: 'bold' }}>{ item }</Text>
+                </TouchableOpacity>
+            ))}
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    itemStyle: {
+        width: '33%',
+        alignItems: 'center',
+        borderWidth: 3,
+        borderColor: 'white',
+        borderRadius: 10,
+        padding: 5,
+        marginVertical: 20,
+    }
+})
+
